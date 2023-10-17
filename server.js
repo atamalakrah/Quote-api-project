e363338f0ba765a70fdfbd4f332cf796813cfba8
Jake Gabriel <73751365+atamalakrah@users.noreply.github.com>
2023-10-16 20:07:17 -0400

    get-quotes
    Get request for a specific person. If no person is included, returns all quotes

--- server.js
+++ server.js
@@ -8,11 +8,31 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+//Request for a random quote
 app.get('/api/quotes/random', (req, res, next) => {
     const quotesFromData = quotes;
     const randomQuote = getRandomElement(quotesFromData);
     const jsonQuote = JSON.stringify({quote: randomQuote});
     res.send(jsonQuote);
-})
+});
+
+//Request for a specific person. If no person is included, returns all quotes.
+app.get('/api/quotes', (req, res, next) => {
+    let tempQuotes = []
+    const personParam = req.query.person;
+    if(!personParam){
+        const jsonAllQuotes = JSON.stringify({quotes: quotes});
+        res.send(jsonAllQuotes);
+    }
+    else{
+        for(let i = 0; i < quotes.length; i++){
+            if(quotes[i].person == personParam){
+                tempQuotes.push(quotes[i]);
+            }
+        }
+        const jsonAllQuotes = JSON.stringify({quotes: tempQuotes});
+        res.send(jsonAllQuotes);
+    }
+});
 
 app.listen(4001, () => console.log("Simple server running on http://localhost:4001"))
